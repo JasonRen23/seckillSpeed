12c7a5cf78ecf3e626f567a41b5a0af38f428b06
jasonren <zhicheng_ren@163.com>
2018-08-22 22:07:35 +0800

    :boy: 添加用户实体类和dao接口

---
+++ src/main/java/com/jasonren/seckill/domain/SeckillUser.java
@@ -0,0 +1,33 @@
+package com.jasonren.seckill.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class SeckillUser {
+    private Long id;
+    private String nickname;
+    private String password;
+    private String salt;
+    private String head;
+    private Date registerDate;
+    private Date lastLoginDate;
+    private Integer loginCount;
+
+
+    @Override
+    public String toString() {
+        return "SeckillUser{" +
+            "id=" + id +
+            ", nickname='" + nickname + '\'' +
+            ", password='" + password + '\'' +
+            ", salt='" + salt + '\'' +
+            ", head='" + head + '\'' +
+            ", tegisterDate=" + registerDate +
+            ", lastLoginDate=" + lastLoginDate +
+            ", loginCount=" + loginCount +
+            '}';
+    }
+
+}

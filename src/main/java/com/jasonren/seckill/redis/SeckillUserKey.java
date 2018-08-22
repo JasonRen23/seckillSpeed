b3a1ad6426cb78debad709f5732cc3b33ed6227f
jasonren <zhicheng_ren@163.com>
2018-08-22 22:09:29 +0800

    每次登陆更新Cookie过期时间

---
+++ src/main/java/com/jasonren/seckill/redis/SeckillUserKey.java
@@ -0,0 +1,12 @@
+package com.jasonren.seckill.redis;
+
+public class SeckillUserKey extends BasePrefix{
+
+    public static final int TOKEN_EXPRIRE = 3600 * 24 * 2;
+
+    private SeckillUserKey(int expireSeconds, String prefix) {
+        super(expireSeconds, prefix);
+    }
+
+    public static SeckillUserKey token = new SeckillUserKey(TOKEN_EXPRIRE, "tk");
+}

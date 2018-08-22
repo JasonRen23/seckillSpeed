cd0c189dea369223af4320aa31cf8671afb661d2
jasonren <zhicheng_ren@163.com>
2018-08-22 22:03:20 +0800

    :page_with_curl: 添加登录Controller

---
+++ src/main/java/com/jasonren/seckill/controller/LoginController.java
@@ -0,0 +1,45 @@
+package com.jasonren.seckill.controller;
+
+import com.jasonren.seckill.redis.RedisService;
+import com.jasonren.seckill.result.Result;
+import com.jasonren.seckill.service.SeckillUserService;
+import com.jasonren.seckill.vo.LoginVo;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    private static Logger log = LoggerFactory.getLogger(LoginController.class);
+    @Autowired
+    private RedisService redisService;
+
+    @Autowired
+    private SeckillUserService userService;
+
+    @ApiOperation("获取登录界面接口")
+    @RequestMapping("/to_login")
+    public String toLogin() {
+        return "login";
+    }
+
+    @ApiOperation("登录接口")
+    @RequestMapping("/do_login")
+    @ResponseBody
+    public Result<Boolean> doLogin(HttpServletResponse response, @Valid LoginVo loginVo) {
+        log.info(loginVo.toString());
+        //登录
+        userService.login(response, loginVo);
+        return Result.success(true);
+    }
+
+
+}

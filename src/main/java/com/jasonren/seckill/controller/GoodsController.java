568d4060830d29d78c361b341abb8908a65269fa
jasonren <zhicheng_ren@163.com>
2018-08-27 00:35:53 +0800

    :tada: 添加商品详情页的Controller类 & 秒杀操作的Controller

--- src/main/java/com/jasonren/seckill/controller/GoodsController.java
+++ src/main/java/com/jasonren/seckill/controller/GoodsController.java
@@ -3,16 +3,25 @@ package com.jasonren.seckill.controller;
 
 import com.jasonren.seckill.domain.SeckillUser;
 import com.jasonren.seckill.redis.RedisService;
+import com.jasonren.seckill.service.GoodsService;
 import com.jasonren.seckill.service.SeckillUserService;
+import com.jasonren.seckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 @Controller
 @RequestMapping("/goods")
 public class GoodsController {
+
+    @Autowired
+    GoodsService goodsService;
+
     @Autowired
     SeckillUserService seckillUserService;
 
@@ -22,7 +31,43 @@ public class GoodsController {
     @RequestMapping("/to_list")
     public String toLogin(Model model, SeckillUser user) {
         model.addAttribute("user", user);
+        //查询商品列表
+        List<GoodsVo> goodsList = goodsService.listGoodsVo();
+        model.addAttribute("goodsList", goodsList);
         return "goods_list";
+    }
+
+
+    @RequestMapping("/to_detail/{goodsId}")
+    public String toLogin(Model model, SeckillUser user,
+                          @PathVariable("goodsId") long goodsId) {
+
+        model.addAttribute("user", user);
+        GoodsVo goods = goodsService.getGoodsVOById(goodsId);
+        model.addAttribute("goods", goods);
+
+        long startAt = goods.getStartTime().getTime();
+        long endAt = goods.getEndTime().getTime();
+        long now = System.currentTimeMillis();
+
+        int seckillStatus = 0;
+        int remainSeconds = 0;
+
+        if (now < startAt) {       // 秒杀还没开始 倒计时
+            seckillStatus = 0;
+            remainSeconds = (int) ((startAt - now) / 1000);
+        } else if (now > endAt) { //秒杀已经结束
+            seckillStatus = 2;
+            remainSeconds = -1;
+        } else {                 //秒杀进行中
+            seckillStatus = 1;
+            remainSeconds = 0;
+        }
+
+        model.addAttribute("seckillStatus", seckillStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+
+        return "goods_detail";
     }
 
 

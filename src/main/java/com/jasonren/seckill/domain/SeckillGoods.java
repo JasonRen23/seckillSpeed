28e01e5b554e3effc083a7240066475bd196b507
jasonren <zhicheng_ren@163.com>
2018-08-27 00:32:31 +0800

    :tada: 添加商品、秒杀商品、订单信息、秒杀订单的domain对象

---
+++ src/main/java/com/jasonren/seckill/domain/SeckillGoods.java
@@ -0,0 +1,26 @@
+package com.jasonren.seckill.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class SeckillGoods {
+    private Long id;
+    private Long goodsId;
+    private Double seckillPrice;
+    private Integer stockCount;
+    private Date startTime;
+    private Date endTime;
+
+    @Override
+    public String toString() {
+        return "SeckillGoods{" +
+            "id=" + id +
+            ", goodsid=" + goodsId +
+            ", stockCount=" + stockCount +
+            ", startTime=" + startTime +
+            ", endTime=" + endTime +
+            '}';
+    }
+}
---
+++ src/main/java/com/jasonren/seckill/domain/SeckillOrder.java
@@ -0,0 +1,21 @@
+package com.jasonren.seckill.domain;
+
+import lombok.Data;
+
+@Data
+public class SeckillOrder {
+    private Long id;
+    private Long userId;
+    private Long orderId;
+    private Long goodsId;
+
+    @Override
+    public String toString() {
+        return "SeckillOrder{" +
+            "id=" + id +
+            ", userId=" + userId +
+            ", orderId=" + orderId +
+            ", goodsId=" + goodsId +
+            '}';
+    }
+}
